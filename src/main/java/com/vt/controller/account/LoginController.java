a64baf26e24f2d433434b77621dceb5356695884
LeThiNhatAnh <lethinhatanh1060@gmail.com>
2024-07-16 08:18:49 +0700

    CẬP NHẬT
    cập nhật login controller

--- src/main/java/com/vt/controller/account/LoginController.java
+++ src/main/java/com/vt/controller/account/LoginController.java
@@ -1,18 +1,45 @@
 package com.vt.controller.account;
 
+import com.vt.model.Users;
+import com.vt.repository.account.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class LoginController {
 
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @GetMapping("/login")
-    public String showLoginForm(@RequestParam(value = "error", required = false) String error, Model model) {
-        if (error != null) {
-            model.addAttribute("errorMessage", "Tên người dùng hoặc mật khẩu không đúng.");
+    public String login() {
+        return "login"; // Trả về trang login.html
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password, Model model) {
+        Optional<Users> userOptional = userRepository.findByUsername(username);
+
+        if (userOptional.isPresent()) {
+            Users user = userOptional.get();
+            if (passwordEncoder.matches(password, user.getPassword())) {
+                // Đăng nhập thành công
+                model.addAttribute("successMessage", "Đăng nhập thành công!"); // Thêm thông báo thành công
+                return "login"; // Trả về lại trang login với thông báo thành công
+            }
         }
-        return "login"; // Trả về tên template login.html
+
+        // Đăng nhập thất bại
+        model.addAttribute("error", true);
+        return "login"; 
     }
 }
